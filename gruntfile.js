e744da5b5482c6a0df3595775ca2fcf21df6d3b0
Makis Tracend <makis.tracend@gmail.com>
2013-03-16 20:48:26 -0700

    moving gruntfile locally

---
+++ gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+	/*
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		cdn: {
+			dist: {
+				src: ['./static/*.html', './static/*.css', './static/*.soy'],
+				dest: './dist/static/',
+				cdn: 'http://cdn.cloudfront.net/container/'
+			}
+		}
+	});
+	*/
+	
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-less');
+	
+};
